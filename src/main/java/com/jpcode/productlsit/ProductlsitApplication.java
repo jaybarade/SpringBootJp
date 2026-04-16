90503dc4e2f942693f89dfc40440b86e94760357
jaybarade <YOUR_GITHUB_EMAIL>
2026-04-16 11:54:57 +0530

    Added Spring Boot project

---
+++ src/main/java/com/jpcode/productlsit/ProductlsitApplication.java
@@ -0,0 +1,13 @@
+package com.jpcode.productlsit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductlsitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductlsitApplication.class, args);
+	}
+
+}
